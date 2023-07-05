4aad7a213ef96f6b10a032311a9d93162421bde0
trkck <trk.kck95@gmail.com>
2023-07-05 13:51:22 +0300

    added adding file

--- src/components/BookstoreAdmin.vue
+++ src/components/BookstoreAdmin.vue
@@ -6,18 +6,18 @@
                 <BootstrapInputText label="Isbn" value="book.isbn">
                     <button class="btn btn-primary">Find Book</button>
                 </BootstrapInputText>
-                <BootstrapInputText label="Title" valuee="book.title"></BootstrapInputText>
-                <BootstrapInputText label="Author" valuee="book.author"></BootstrapInputText>
-                <BootstrapInputText label="Price" valuee="book.price"></BootstrapInputText>
-                <BootstrapInputText label="Year" valuee="book.year"></BootstrapInputText>
-                <BootstrapInputText label="Pages" valuee="book.pages"></BootstrapInputText>
-                <BootstrapInputText label="Publisher" valuee="book.publisher"></BootstrapInputText>
+                <BootstrapInputText label="Title" @change="book.title=$event.target.value" :value="book.title"></BootstrapInputText>
+                <BootstrapInputText label="Author" @change="book.author=$event.target.value" :value="book.author"></BootstrapInputText>
+                <BootstrapInputText label="Price" @change="book.price=$event.target.value" :value="book.price"></BootstrapInputText>
+                <BootstrapInputText label="Year" @change="book.year=$event.target.value" :value="book.year"></BootstrapInputText>
+                <BootstrapInputText label="Pages" @change="book.pages=$event.target.value" :value="book.pages"></BootstrapInputText>
+                <BootstrapInputText label="Publisher" @change="book.publisher=$event.target.value" :value="book.publisher"></BootstrapInputText>
                 <div class="input-group">
                     <label class="form-label">Cover</label>
-                    <img id="cover">
+                    <img id="cover" v-bind:src="book.cover">
                     <input type="file" class="form-control">
                     <label>
-                        <input type="file" style="display: none" class="form-control">
+                        <input type="file" style="display: none" v-on:change="selectFile"  class="form-control">1
                         <span class="btn btn-info">File</span>
                     </label>
                 </div>
@@ -45,6 +45,17 @@ export default {
     props: {
     msg: String
   },
+    methods:{
+      selectfile(event){
+          let file = event.target.files[0];
+          let reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = (event) => {
+              this.book.cover = event.target.result;
+          }
+      }
+    }
+    ,
     data:function () {
       return{
           book: new Book()
--- src/components/BootstrapInputText.vue
+++ src/components/BootstrapInputText.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mb-3">
         <label class="form-label card-text mb-2" :for="label">{{label}}</label>
-        <input type="text" :id="label" class="form-control mb-2">
+        <input type="text" :id="label" :value="value" class="form-control mb-2">
         <slot></slot>
     </div>
 </template>
@@ -9,7 +9,7 @@
 <script>
     export default {
         name: "BootstrapInputText",
-        props: ["label","valuee"]
+        props: ["label","value"]
     }
 </script>
 
